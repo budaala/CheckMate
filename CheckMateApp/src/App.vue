82d9302e84263eb10c020378fd92a4732013e0c7
budaala <budaala4@gmail.com>
2024-05-05 13:44:20 +0200

    initial frontend

---
+++ CheckMateApp/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+        expand-on-hover
+        rail
+        v-model="drawer"
+        app
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            subtitle="sandra_a88@gmailcom"
+            title="Sandra Adams"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
